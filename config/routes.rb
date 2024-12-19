fe02a6dd38e2365f5a974785f6fd01bdef0f8590
Clement MOLLET <mollet.clement.dev@gmail.com>
2024-12-19 18:17:43 +0100

    advance front + carrousel

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  get 'pages/nous-contacter', to: "pages#contact", as: "contact"
-  get 'pages/reserver', to: "pages#booking", as: "booking"
+  get '/nous-contacter', to: "pages#contact", as: "contact"
+  get '/reserver', to: "pages#booking", as: "booking"
+  get '/conditions', to: "pages#conditions", as: "conditions"
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "pages#home"
